3cc5aaf2bdd19ebbcebad14bd8f3a36d53e5f3b9
okradonkey <okradonkey@disroot.org>
2020-04-27 07:02:31 -0500

    move comments

--- Source/Truly Inspired Taming/TrulyInspiredTaming.cs
+++ Source/Truly Inspired Taming/TrulyInspiredTaming.cs
@@ -4,18 +4,18 @@ using RimWorld;
 using Verse;
 using Verse.AI;
 
+// Colonists with Inspired Taming can now tame animals beyond their current skill level.
+// Mod settings provide three options to determine how far beyond:
+//   - Tame any animal
+//   - Boost by percentage
+//   - Boost by levels
+//
+// This does not change a colonist's actual Animals skill level.
+
+// Strategy: Postfix WorkGiver_InteractAnimal.CanInteractWithAnimal
+
 namespace TrulyInspiredTaming
 {
-    // Colonists with Inspired Taming can now tame animals beyond their current skill level.
-    // Mod settings provide three options to determine how far beyond:
-    //   - Tame any animal
-    //   - Boost by percentage
-    //   - Boost by levels
-    //
-    // This does not change a colonist's actual Animals skill level.
-
-    // Strategy: Postfix WorkGiver_InteractAnimal.CanInteractWithAnimal
-
     [StaticConstructorOnStartup]
     internal static class HarmonyPatches
     {
