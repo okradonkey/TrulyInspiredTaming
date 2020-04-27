f0799d2f7a00c2855ff012e4d6f5c70d8f7fc3b1
okradonkey <okradonkey@disroot.org>
2020-04-26 20:42:17 -0500

    begin using autoversion

--- Source/Truly Inspired Taming/Properties/AssemblyInfo.cs
+++ Source/Truly Inspired Taming/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TrulyInspiredTaming")]
+[assembly: AssemblyTitle("Truly Inspired Taming")]
 [assembly: AssemblyDescription("RimWorld mod")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TrulyInspiredTaming")]
+[assembly: AssemblyProduct("Truly Inspired Taming")]
 [assembly: AssemblyCopyright("Copyright © 2020 OkraDonkey")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("0.2.3.3")]
+[assembly: AssemblyFileVersion("0.2.5.20117")]
